463b7e8f569d0f09f4154ff1d57a46a393fccb18
Jacopo Carlon <73998123+JacopoCarlon@users.noreply.github.com>
2024-06-05 12:22:29 +0200

    Update top-languages-fetcher.js : languages(first: 80,

--- src/fetchers/top-languages-fetcher.js
+++ src/fetchers/top-languages-fetcher.js
@@ -30,7 +30,7 @@ const fetcher = (variables, token) => {
           repositories(ownerAffiliations: OWNER, isFork: false, first: 100) {
             nodes {
               name
-              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+              languages(first: 80, orderBy: {field: SIZE, direction: DESC}) {
                 edges {
                   size
                   node {
